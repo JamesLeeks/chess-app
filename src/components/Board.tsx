4db68f2a90f328891a6fa587097274410a28c707
James Leeks <james@leeks.net>
2024-04-02 11:22:59 +0100

    move click handling and getting moves from board to game

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,62 +1,19 @@
-import React, { useState } from "react";
 import { Square } from "./Square";
-import { getMoveOptions } from "../core/getMoveOptions";
-import { Board, Position, PieceColour, SquareType } from "../core/models";
+import { Board, Position, SquareType } from "../core/models";
 
 export type BoardComponentParams = {
 	board: Board;
-	makeMove: (from: Position, to: Position) => void;
+	// TODO: get row and column from square: Position
+	handleClick: (row: number, column: number) => void;
+	selectedSquare: Position | null;
+	moveOptions: Position[];
 };
-export function BoardComponent({ board, makeMove }: BoardComponentParams) {
-	const [selectedSquare, setSelectedSquare] = useState<Position | null>(null);
-	const [moveOptions, setMoveOptions] = useState<Position[]>([]);
-	const [currentTurn, setCurrentTurn] = useState<PieceColour>("white");
-
-	// TODO: move click handling up to the GameComponent
-	function handleClick(row: number, column: number) {
-		if (selectedSquare) {
-			// there  a selected piece - move it
-			if (board[row][column]?.colour === currentTurn) {
-				// if it's their turn:
-				const newSelectedSquare = { row: row, column: column };
-				setSelectedSquare({ row: row, column: column });
-				setMoveOptions(getMoveOptions(newSelectedSquare, board));
-			} else {
-				// note see foundMoveOptions
-				const foundMoveOption = moveOptions.find(
-					(pos) => pos.row === row && pos.column === column
-				);
-				if (foundMoveOption) {
-					makeMove(selectedSquare, { row: row, column: column });
-				} else {
-					setSelectedSquare(null);
-					setMoveOptions([]);
-					return;
-				}
-				setSelectedSquare(null);
-				setMoveOptions([]);
-				// switch turn
-				if (currentTurn === "white") {
-					setCurrentTurn("black");
-				} else {
-					setCurrentTurn("white");
-				}
-			}
-		} else {
-			// picking a piece
-			// if there is a piece on this square and it's their turn
-			if (board[row][column] && currentTurn === board[row][column]?.colour) {
-				const newSelectedSquare = { row: row, column: column };
-				setSelectedSquare(newSelectedSquare);
-				setMoveOptions(getMoveOptions(newSelectedSquare, board));
-			} else {
-				setSelectedSquare(null);
-				setMoveOptions([]);
-				return;
-			}
-		}
-	}
-
+export function BoardComponent({
+	board,
+	handleClick,
+	selectedSquare,
+	moveOptions,
+}: BoardComponentParams) {
 	const boardContent = [];
 	// for each row:
 	for (let rowIndex = 0; rowIndex < 8; rowIndex++) {
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,17 +2,73 @@ import { useState } from "react";
 import { BoardComponent } from "./Board";
 import { Game } from "../core/game";
 import { getStartingBoard } from "../core/board";
-import { Position } from "../core/models";
+import { PieceColour, Position } from "../core/models";
 
 export function GameComponent() {
 	const [game, setGame] = useState<Game>(new Game(getStartingBoard()));
+	const [currentTurn, setCurrentTurn] = useState<PieceColour>("white");
+	const [selectedSquare, setSelectedSquare] = useState<Position | null>(null);
+	const [moveOptions, setMoveOptions] = useState<Position[]>([]);
 
 	function makeMove(from: Position, to: Position) {
 		setGame(game.makeMove(from, to));
 	}
+
+	function handleClick(row: number, column: number) {
+		if (selectedSquare) {
+			// there  a selected piece - move it
+			if (game.board[row][column]?.colour === currentTurn) {
+				// if it's their turn:
+				const newSelectedSquare = { row: row, column: column };
+				setSelectedSquare({ row: row, column: column });
+				setMoveOptions(game.getMoveOptions(newSelectedSquare));
+			} else {
+				// note see foundMoveOptions
+				const foundMoveOption = moveOptions.find(
+					(pos) => pos.row === row && pos.column === column
+				);
+				if (foundMoveOption) {
+					makeMove(selectedSquare, { row: row, column: column });
+				} else {
+					setSelectedSquare(null);
+					setMoveOptions([]);
+					return;
+				}
+				setSelectedSquare(null);
+				setMoveOptions([]);
+				// switch turn
+				if (currentTurn === "white") {
+					setCurrentTurn("black");
+				} else {
+					setCurrentTurn("white");
+				}
+			}
+		} else {
+			// picking a piece
+			// if there is a piece on this square and it's their turn
+			if (
+				game.board[row][column] &&
+				currentTurn === game.board[row][column]?.colour
+			) {
+				const newSelectedSquare = { row: row, column: column };
+				setSelectedSquare(newSelectedSquare);
+				setMoveOptions(game.getMoveOptions(newSelectedSquare));
+			} else {
+				setSelectedSquare(null);
+				setMoveOptions([]);
+				return;
+			}
+		}
+	}
+
 	return (
 		<>
-			<BoardComponent board={game.board} makeMove={makeMove} />
+			<BoardComponent
+				board={game.board}
+				handleClick={handleClick}
+				moveOptions={moveOptions}
+				selectedSquare={selectedSquare}
+			/>
 		</>
 	);
 }
