2e16142ae1aca16e0b7f875b8af4fcc31de4ec7b
James Leeks <james@leeks.net>
2024-06-01 12:33:37 +0100

    change the starting board back to the right value

--- src/core/board.tsx
+++ src/core/board.tsx
@@ -8,44 +8,15 @@ import {
 } from "./models";
 
 export function getStartingBoard(): Board {
-	// return [
-	// 	[
-	// 		{ colour: "black", type: "rook" },
-	// 		{ colour: "black", type: "knight" },
-	// 		{ colour: "black", type: "bishop" },
-	// 		{ colour: "black", type: "queen" },
-	// 		{ colour: "black", type: "king" },
-	// 		{ colour: "black", type: "bishop" },
-	// 		{ colour: "black", type: "knight" },
-	// 		{ colour: "black", type: "rook" },
-	// 	],
-	// 	Array(8).fill({ colour: "black", type: "pawn" }),
-	// 	Array(8).fill(undefined),
-	// 	Array(8).fill(undefined),
-	// 	Array(8).fill(undefined),
-	// 	Array(8).fill(undefined),
-	// 	Array(8).fill({ colour: "white", type: "pawn" }),
-	// 	[
-	// 		{ colour: "white", type: "rook" },
-	// 		{ colour: "white", type: "knight" },
-	// 		{ colour: "white", type: "bishop" },
-	// 		{ colour: "white", type: "queen" },
-	// 		{ colour: "white", type: "king" },
-	// 		{ colour: "white", type: "bishop" },
-	// 		{ colour: "white", type: "knight" },
-	// 		{ colour: "white", type: "rook" },
-	// 	],
-	// ];
-
 	return parseBoard(`
-	BK -- -- -- -- -- -- --
-	-- -- -- -- -- -- -- WP
+	BR BN BB BQ BK BB BN BR
+	BP BP BP BP BP BP BP BP
 	-- -- -- -- -- -- -- --
 	-- -- -- -- -- -- -- --
 	-- -- -- -- -- -- -- --
 	-- -- -- -- -- -- -- --
-	-- -- -- -- -- -- -- BP
-	WK -- -- -- -- -- -- --
+	WP WP WP WP WP WP WP WP
+	WR WN WB WQ WK WB WN WR
 	`);
 }
 
