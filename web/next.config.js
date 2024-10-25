1999c02af1e12445630095e70488136c9a7ccb99
James Leeks <james@leeks.net>
2024-10-25 09:46:31 +0100

    next js build steps

--- web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    distDir: 'dist',
-    // output: 'export'
-};
-
-module.exports = nextConfig;
--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'dist',
+    output: 'export'
+};
 
 export default nextConfig;
