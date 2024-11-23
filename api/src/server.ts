d8df9dfdb1df648e68978dc05d40d915ffa9e3ed
James Leeks <james@leeks.net>
2024-11-23 10:53:03 +0000

    add route api response

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,4 +6,8 @@ app.use(cors()); // TODO - limit to specific origins
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+	res.send("Hello!");
+});
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
